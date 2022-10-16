06635da896f595d0282b91afd52da0e956fb62e4
syc <2582410772@qq.com>
2022-10-16 16:06:02 +0800

    foreignkey-step1

---
+++ app/admin/models/user_patent.go
@@ -0,0 +1,24 @@
+package models
+
+import "go-admin/common/models"
+
+type SysUserPatent struct {
+	models.Model
+	PatentId int    `json:"PatentId" gorm:"size:128;comment:专利ID"`
+	UserId   int    `gorm:"primaryKey;autoIncrement;comment:编码"  json:"userId"`
+	ID       int    `json:"ID" gorm:"size:128;comment:关系ID"`
+	Type     string `json:"Type" gorm:"size:128;comment:关系类型（关注/认领）"`
+	models.ControlBy
+}
+
+func (e *SysUserPatent) TableName() string {
+	return "user_patent"
+}
+
+func (e *SysUserPatent) GetUserId() interface{} {
+	return e.UserId
+}
+
+func (e *SysUserPatent) GetPatentId() interface{} {
+	return e.PatentId
+}
