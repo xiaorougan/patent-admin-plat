7d84bbe791b11764c1c4e1bc3a8afe4dd0ed7839
syc <2582410772@qq.com>
2022-10-16 18:41:27 +0800

    change-primary-key-of-jointables

--- app/patent/models/user_patent.go
+++ app/patent/models/user_patent.go
@@ -4,10 +4,10 @@ import "go-admin/common/models"
 
 type SysUserPatent struct {
 	models.Model
-	PatentId int    `gorm:"foreignKey:PatentId" json:"PatentId" `
-	UserId   int    `gorm:"primaryKey;autoIncrement;comment:编码"  json:"userId"`
-	ID       int    `json:"ID" gorm:"size:128;comment:关系ID"`
-	Type     string `json:"Type" gorm:"size:128;comment:关系类型（关注/认领）"`
+	PatentId int    `gorm:"foreignKey:PatentId;comment:PatentId" json:"PatentId" `
+	UserId   int    `gorm:"comment:用户ID"  json:"userId"`
+	ID       int    `gorm:"primaryKey;autoIncrement;comment:编码" json:"ID" `
+	Type     string `json:"Type" gorm:"size:64;comment:关系类型（关注/认领）"`
 	models.ControlBy
 }
 
